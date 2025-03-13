9c788e881040b080020425711431d40960666268
stu.chen <best_css@foxmail.com>
2025-03-13 16:19:36 +0800

    update: project start success

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  }
+  app.enableCors(corsOptions)
+  await app.listen(process.env.PORT ?? 3000)
+  console.log(`Server running on port ${process.env.PORT ?? 3000}`)
 }
-bootstrap();
+void bootstrap()
